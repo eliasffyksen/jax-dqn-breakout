0675ddf9a730c5248142f423887c3f694c2d99b4
Elias F. Fyksen <github@fyksen.net>
2023-10-29 15:04:33 +0000

    Use custom baseline3 env wrappers

--- src/train.py
+++ src/train.py
@@ -10,9 +10,11 @@ from wdb import get_wdb
 from agent import Agent
 from replay_buffer import ReplayBuffer
 from policy import NatureCNN
-from atari_wrappers import wrap_deepmind
 from wandb_wrapper import WandbWrapper
 from profiler import Profiler
+from env.atari_wrappers import AtariWrapper
+from env.vec_env.dummy_vec_env import DummyVecEnv
+from env.vec_env.vec_frame_stack import VecFrameStack
 
 @dataclass
 class Hyperparameters:
@@ -28,10 +30,11 @@ class Hyperparameters:
     seed: int = 42
     train_length: int = 5_000_000
     log_interval: int = 1000
-    eps_min: float = 0.05
+    eps_min: float = 0.01
     eps_max: float = 1.0
     eps_decay_len: float = 1_000_000
     start_learning: int = 100_000
+    frame_stack: int = 4
 
 def convert_state(state: jax.Array) -> jax.Array:
     return state.astype(jnp.float32) / 255.0
@@ -56,13 +59,16 @@ optimiser = optax.chain(
 
 env = gym.make(
     "ALE/Breakout-v5",
-    frameskip=4,
+    frameskip=1,
     repeat_action_probability=0.0,
 )
-env = WandbWrapper(env, wdb, 4000)
-env = wrap_deepmind(env, frame_stack=4)
-state, _ = env.reset()
-state = jnp.array(state)
+if wdb is not None:
+    env = WandbWrapper(env, wdb, 4000)
+env = AtariWrapper(env)
+env = DummyVecEnv([lambda: env])
+env = VecFrameStack(env, hyperparameters.frame_stack)
+state = env.reset()
+state = jnp.array(state[0])
 
 agent = Agent(
     policy=policy,
@@ -86,7 +92,7 @@ profiler = Profiler()
 for step in range(hyperparameters.train_length):
 
     if step % hyperparameters.rollout_freq == 0:
-        profiler.enter('rollout')
+        profiler.enter('rollout.action_selection')
 
         rng, eps_rng, action_rng = jax.random.split(rng, 3)
         eps = calculate_epsilon(step, hyperparameters)
@@ -96,8 +102,10 @@ for step in range(hyperparameters.train_length):
             action = agent.predict(convert_state(state)[None])[0]
             action = action.argmax()
 
-        next_state, reward, terminal, _, _ = env.step(action)
-        next_state = jnp.array(next_state)
+        profiler.enter('rollout.env_step')
+        next_state, reward, terminal, _ = env.step([action])
+        next_state = jnp.array(next_state[0])
+        profiler.enter('rollout.save_to_buffer')
         replay_buffer.push(
             states=state,
             actions=action,
@@ -107,9 +115,10 @@ for step in range(hyperparameters.train_length):
         )
         state = next_state
 
+        profiler.enter('rollout.reset')
         if terminal:
-            state, _ = env.reset()
-            state = jnp.array(state)
+            state = env.reset()
+            state = jnp.array(state[0])
 
     if step % hyperparameters.train_freq == 0:
         profiler.enter('train')
